24a588a2426379ddd22095f2d99c230d8c046696
juanluna96 <juandluna17@gmail.com>
2023-05-22 16:48:39 -0500

    :sparkles: Adding particle background

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,32 @@
 import React from "react";
+import ParticleBackground from "./particles/ParticleBackground";
+import {
+  BodyContainer,
+  FontFamilyContainer,
+  FormCardContainer,
+  MiddleCard,
+} from "@/assets/styles/formCard.style";
 
 interface LayoutProps {
   children: React.ReactNode;
+  error: boolean;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children, error = false }) => {
   return (
-    <div>
-      <header>
-        {/* Coloca aquí el encabezado de tu página */}
-        Header
-      </header>
-      <main>{children}</main>
-      <footer>
-        Footer
-        {/* Coloca aquí el pie de página de tu página */}
-      </footer>
-    </div>
+    <FontFamilyContainer>
+      <div id="modal-root"></div>
+      <BodyContainer>
+        <div style={{ position: "absolute" }}>
+          <ParticleBackground error={error} />
+        </div>
+        <MiddleCard>
+          <FormCardContainer>
+            <main>{children}</main>
+          </FormCardContainer>
+        </MiddleCard>
+      </BodyContainer>
+    </FontFamilyContainer>
   );
 };
 
---
+++ src/components/particles/ParticleBackground.tsx
@@ -0,0 +1,28 @@
+import { useCallback } from "react";
+import type { Engine } from "tsparticles-engine";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import particleshome from "@/utils/particleshome.json";
+import particlesnotfound from "@/utils/particlesnotfound.json";
+
+interface ComponentProps {
+  error: boolean;
+}
+
+const ParticleBackground: React.FC<ComponentProps> = ({ error }) => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <Particles
+      height="100vh"
+      width="100vw"
+      id="tsparticles"
+      init={particlesInit}
+      options={error ? particlesnotfound : particleshome}
+    />
+  );
+};
+
+export default ParticleBackground;
