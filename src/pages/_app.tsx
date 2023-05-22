560e0579be82c962a308972dc5f47555c344b201
juanluna96 <juandluna17@gmail.com>
2023-05-22 16:54:57 -0500

    :art: Added icon to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "@/redux/store";
+import "../assets/main.css";
+import Head from "next/head";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <title>Habi - Registra tu apartamento</title>
+        <link rel="shortcut icon" href="/images/icon.ico" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
   );
-}
+};
 
 export default MyApp;
